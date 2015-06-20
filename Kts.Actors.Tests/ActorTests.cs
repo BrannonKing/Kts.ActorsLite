08650d9b3dc7036893ef8de2e899c55ec4d114cb
Brannon King <countprimes@gmail.com>
2015-06-20 10:30:31 -0600

    added a few more tests

--- Kts.Actors.Tests/ActorTests.cs
+++ Kts.Actors.Tests/ActorTests.cs
@@ -23,6 +23,7 @@ namespace Kts.Actors.Tests
 		// 2. make sure an exception on 1, 3, or 5 of 5 still lets the others run
 		// 3. cancelling before it runs or during a run should return cancelled tasks
 		// 4. make sure the four that should keep them in order do
+		// 5. make sure each actor can handle multiple threads pushing to it
 
 		private IEnumerable<IActor<T, R>> CreateActors<T, R>(Func<T, CancellationToken, R> func)
 		{
@@ -77,6 +78,80 @@ namespace Kts.Actors.Tests
 
 
 			}
+		}
+
+		[Fact]
+		public void TestMiddleSkipped()
+		{
+			var hits = new bool[3];
+
+			Action<int, CancellationToken> t = (i, tok) =>
+			{
+				hits[i] = true;
+				Thread.Sleep(20);
+			};
+
+			var queue = new MostRecentAsyncActor<int>(t);
+			queue.Push(0);
+			queue.Push(1);
+			queue.Push(2).Wait();
+
+			// the hitFirst value is arbitrary (based on timing) and not particularly related to the goal
+			Assert.False(hits[1]);
+			Assert.True(hits[2]);
+		}
+
+		[Fact]
+		public void TestRobustness()
+		{
+			var rand = new Random(42);
+			int lastAction = -1, lastAction2 = 42;
+			Action<int, CancellationToken> t = (r, tok) =>
+			{
+				lastAction = r;
+				Thread.Sleep(rand.Next(100));
+			};
+			var queue = new MostRecentAsyncActor<int>(t);
+			for (var i = 0; i < 1000; i++)
+			{
+				var r = rand.Next();
+				lastAction2 = r;
+
+				var task = queue.Push(r);
+				if (i == 999)
+					task.Wait();
+				else
+					Thread.Sleep(rand.Next(150));
+			}
+
+			Assert.Equal(lastAction, lastAction2);
+		}
+
+		[Fact]
+		public void TestMostRecentSubAdd()
+		{
+			// queue some tasks that take 200ms 100ms apart
+			var hits = new bool[10];
+			Action<int, CancellationToken> t = (r, tok) =>
+			{
+				if (!tok.IsCancellationRequested)
+				{
+					hits[r] = true;
+					Thread.Sleep(100);
+				}
+			};
+
+			var queue = new MostRecentAsyncActor<int>(t);
+			Task last = null;
+			for (var i = 0; i < hits.Length; i++)
+			{
+				last = queue.Push(i);
+				Thread.Sleep(45);
+			}
+			if (last != null)
+				last.Wait();
+
+			Assert.True(hits.Last());
 		}
 	}
 }
